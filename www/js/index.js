c0021ab548612c434fc512bae99f7bbb79e45ca8
Iqmah Odufade <ikky@Iqmahs-MacBook-Pro.local>
2022-03-13 22:58:04 +0000

    second commit

--- www/js/index.js
+++ www/js/index.js
@@ -4,6 +4,8 @@ function onDeviceReady() {
     // Cordova is now initialized. Have fun!
    admob.start();
   document.getElementById("bannerButton").addEventListener("click", bannerAdShow);	
+  document.getElementById("intersitialButton").addEventListener("click", interstitialAdShow);	
+  document.getElementById("rewardButton").addEventListener("click", showRewardedAds);	
 }
 function bannerAdShow() {
 let banner
@@ -15,4 +17,36 @@ banner.on('impression',  (evt) => {
  banner.hide()
 })
 banner.show()
+}
+
+
+function interstitialAdShow() {
+  interstitial = new admob.InterstitialAd({
+    adUnitId: 'ca-app-pub-5580919195791472/6081873213',
+  })
+
+  interstitial.on('load', (evt) => {
+    evt.ad
+  })
+   interstitial.load()
+   interstitial.show()
+
+document.addEventListener('admob.ad.dismiss', async () => {
+  await interstitial.load()
+})
+}
+
+
+function showRewardedAds(){
+  let rewarded;
+  rewarded = new admob.RewardedAd({
+    adUnitId: 'ca-app-pub-5580919195791472/6115013870',
+  })
+
+  rewarded.on('load', (evt) => {
+     rewarded.load()
+  })
+
+  await rewarded.load()
+  await rewarded.show()
 }
