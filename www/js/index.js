ef88bb9554af8ddc9f0e17275673499e6d294950
somto <king>
2022-03-14 00:12:39 +0000

    goodbye pr take 1

--- www/js/index.js
+++ www/js/index.js
@@ -1,18 +1,27 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-   admob.start();
-  document.getElementById("bannerButton").addEventListener("click", bannerAdShow);	
+async function onDeviceReady() {
+  // Cordova is now initialized. Have fun!
+  // await admob.start();
+   const ad = new admob.AppOpenAd({
+    adUnitId: 'ca-app-pub-5580919195791472/3987004985',
+  })
+
+    document.getElementById("bannerButton").addEventListener("click", async ()=>{
+      const data = await !ad.show()
+      if (!data) {
+        alert(33)
+         await ad.load()
+      }
+      else{
+        console.log({
+          data
+        })
+      }
+    })
 }
-function bannerAdShow() {
-let banner
-banner = new admob.BannerAd({
-adUnitId: 'ca-app-pub-5580919195791472/3987004985',
-autoShow: true,
+
+
+document.addEventListener('admob.ad.show', async (evnt)=>{
+  console.log({evnt})
 })
-banner.on('impression',  (evt) => {
- banner.hide()
-})
-banner.show()
-}
